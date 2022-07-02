2239c77bbc7342735956684cc3787e7a858e5ac2
Maksim <bagzmax75@gmail.com>
2022-07-02 21:26:12 +0300

    add more Example003 TEXT

---
+++ Example003_text/Program.cs
@@ -0,0 +1,24 @@
+﻿// Дан текст. В тексте нужно все пробелы заменить черточками,
+//маленькие буквы “и” заменить большими “И”, а большие “С” заменить
+//маленькими “с”.
+
+string text = "Дан текст. В тексте нужно все пробелы заменить черточками";
+
+// string s = "qwerty"
+//             0123
+//s[3] // r
+
+string Replase(string text, char oldValue, char newValue) // длина строки, старый символ, новый символ
+{
+    string result = String.Empty;
+
+    int length = text.Length; // количество символов в стоке
+    for (int i = 0; i < length; i++)
+    {
+        if (text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+    return result;
+}
+string newText = Replase(text, ' ', '|');
+System.Console.WriteLine(newText);
