76e504a0869a9c309364f6506be820f4222563fb
Maksim <bagzmax75@gmail.com>
2022-07-03 10:01:41 +0300

    add more Example004 Array Sort

---
+++ Example004_ArraySort/Program.cs
@@ -0,0 +1,16 @@
+﻿//Сортировка массива
+
+int [] arr = {1, 2, 3, 5, 5, 8, 34, 9};
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+} 
+
+PrintArray(arr);
